da432e3f5637e00d1daa2619937062481f28a6cd
marioguio <mario_guio@outlook.com>
2017-09-15 13:38:03 -0500

    finaly

--- check.rb
+++ check.rb
@@ -1,4 +1,5 @@
 require "pg"
+require "redis"
 
 def exit_if_not(expected, current)
   puts "Expected: #{expected}"
@@ -6,8 +7,12 @@ def exit_if_not(expected, current)
   exit(1) if expected != current
 end
 
+puts "Redis"
+redis = Redis.new(host: "redis")
+puts "REDIS VERSION: #{redis.info["redis_version"]}"
+
 sleep 4
-postgres_username = "mario"
-postgres_password = "mario"
-test = PG.connect("mario", 5432, "mario", "mario", "mario", postgres_username, postgres_password)
+postgres_username = "postgres"
+postgres_password = ""
+test = PG.connect("postgres", 5432, "", "", "postgres", postgres_username, postgres_password)
 puts test.exec("SELECT version();").first["version"]
